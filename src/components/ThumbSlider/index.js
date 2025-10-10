7d93c3180d6e22a8755f0e822ed06c4cd4b7ae03
masato koga <danilomasato@hotmail.com>
2025-10-10 19:56:39 -0300

    fix: problema sem imagem no carousel thumbSlider, e adicionado quando nao há imagem

--- src/components/ThumbSlider/index.js
+++ src/components/ThumbSlider/index.js
@@ -8,11 +8,19 @@ const baseURL = process.env.REACT_APP_URL;
 const ThumbSLider = (props) => {
   return (
     <>
-        <CustomSlider>
-        {props.image.map((image, index) => {
-          return <img key={index} src={`${baseURL}${image.url}`} style={{ width: "100%", height: props.height + 'px', objectFit: 'cover' }} alt={image.imgAlt} />;
-        })}
-      </CustomSlider>    
+        {props.image?.length > 0 ? (
+          <CustomSlider>
+            {props.image.map((image, index) => {
+              return <img key={index} src={`${baseURL}${image.url}`} style={{ width: "100%", height: props.height + 'px', objectFit: 'cover' }} alt={image.imgAlt} />;
+            })}
+          </CustomSlider>  
+        )
+      : ( 
+      <>
+        <img src={`https://www2.camara.leg.br/atividade-legislativa/comissoes/comissoes-permanentes/cindra/imagens/sem.jpg.gif/image`} style={{ width: "100%", height: props.height + 'px', objectFit: 'contain' }} alt='sem imagem' />
+      </>
+      )
+      }
     </>
   );
 }
