2e01d038b6eb6e78ab2eaa5d988b5d507f2a6e86
masato koga <danilomasato@hotmail.com>
2025-10-14 21:14:12 -0300

    add footer e title fale conosco

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -11,6 +11,7 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import SendIcon from '@mui/icons-material/Send';
+import Typography from '@mui/material/Typography';
 
 const Contact = ({ props }) => {
 
@@ -22,7 +23,12 @@ const Contact = ({ props }) => {
 
       <div className="row center">
         <div className="content" style={{ minHeight: "auto",  display: "block" }}>
-
+          <Box sx={{ width: '100%' }}>
+            <Typography variant="h5" gutterBottom>
+              Fale Conosco
+            </Typography>
+          </Box>
+          <br />
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.974055769659!2d-46.71446422392879!3d-23.676885778720624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce51cc5a3127fb%3A0x4815a5b3d2fbe069!2sAv.%20do%20Rio%20Bonito%2C%20162%20-%20Socorro%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004776-000!5e0!3m2!1spt-BR!2sbr!4v1760388420153!5m2!1spt-BR!2sbr" width="450" height="450" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           <div className="form">
             <Box
@@ -47,6 +53,8 @@ const Contact = ({ props }) => {
           </div>
         </div>
       </div>
+
+      <Footer />
     </React.Fragment>
   );
 };
