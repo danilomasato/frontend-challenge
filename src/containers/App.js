5c9212d63fa75fffb451a39cd6b034e7224bdbee
masato koga <danilomasato@hotmail.com>
2026-01-19 16:49:18 -0300

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import Home  from "./Home";
 import CharacterDetail from "./CharacterDetail";
@@ -10,15 +10,15 @@ import "./App.css";
 
 const App = ({ store }) => (
   <Provider store={store}>
-    <Router>
-      <Switch>
+    <BrowserRouter>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route exact path="/imovel/:id/:id" component={CharacterDetail} />
         <Route exact path="/contato" component={Contact} />
         <Route exact path="/sobre-nos" component={About} />
         <Route exact path="/Register" component={Register} />
-      </Switch>
-    </Router>
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
 
