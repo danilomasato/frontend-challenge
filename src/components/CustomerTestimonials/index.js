67bbc627e6aa126d1f6e493bafdc138175c4eb7b
masato koga <danilomasato@hotmail.com>
2026-02-17 13:44:28 -0300

    add foto depoimento

--- src/components/CustomerTestimonials/index.js
+++ src/components/CustomerTestimonials/index.js
@@ -25,8 +25,7 @@ export const CustomerTestimonials  = () => {
           <Card className="CustomerTestimonials-card" sx={{ maxWidth: 345 }}>
             <CardHeader
               avatar={
-                <Avatar sx={{ bgcolor: '#ccc' }} aria-label="recipe">
-                  B
+                <Avatar sx={{ backgroundImage: `url('https://tudosobreap.com.br/assets/images/brendaqueiroz.jpg')`, backgroundSize: cover; }} aria-label="recipe">
                 </Avatar>
               }
               title="Brenda Queiroz"
