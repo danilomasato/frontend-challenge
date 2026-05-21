eaa288f50eebfe09afa9288a5aafa897cd59d932
masato koga <masatokoga32@gmail.com>
2026-05-21 17:32:20 -0300

    page size

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import * as utils from "../utils";
 
-export const getCharacterData = (id) => utils.GetAPI(!id ? 'Anuncios' : `Anuncios?pagination[page]=${id}&pagination[pageSize]=30&populate=*`);
+export const getCharacterData = (id) => utils.GetAPI(!id ? 'Anuncios' : `Anuncios?pagination[page]=${id}&pagination[pageSize]=25&populate=*`);
 export const getArticles = (id) => utils.GetAPI( id ? 'Anuncios' : `Anuncios?populate=*`);
 export const getAuthors = (id) => utils.GetAPI( id ? 'Anuncios' : `Anuncios?populate=*`);
 export const getImoveisCache = (id) => utils.GetNewAPI("https://tudosobreap.com.br/Articles.json");
