5346ba6d38ac092afdc7e7f377ee68e745cb5cb8
masato koga <masatokoga32@gmail.com>
2026-05-31 15:57:06 -0300

    refatoração docomponente search esse ta ok

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -175,7 +175,7 @@ const Home = ({ character, imoveisCache, pagination}) => {
             setResearch(minMax(item))
           }
 
-          busca por tipo de anuncio e bairro
+          //busca por tipo de anuncio e bairro
           if(category !== '' && item.Tipo_de_Anuncio.includes(category) && item.Bairro.includes(search?.label)){
             setResearch(minMax(item))
           }
